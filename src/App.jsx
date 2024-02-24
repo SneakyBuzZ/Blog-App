b24392f7326e08a894c74577938483b911b64dfe
SneakyBuzZ <kaushikx304@gmail.com>
2024-02-24 05:34:09 +0530

    Redux Toolkit Enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-
+import { useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react';
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>MEGA BLOG APP</h1>
-    </>
-  )
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading[false])
+  }, [dispatch])
+
+  return loading ? (
+    <div className=" w-full my-10 text-3xl text-center bg-stone-900">MEGA BLOG APP</div>
+  ) : null
 }
 
 export default App
